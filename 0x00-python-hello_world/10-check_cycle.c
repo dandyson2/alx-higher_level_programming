fe6e53914819145a0b4ef4ae7825aa74511b48e8
dandyson2 <dandysonvictor@yahoo.com>
2023-06-05 11:38:45 -0700

    Linked list cycle, Task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * check_cycle - Function that checks if a singly
+ * linked list has a cycle in it
+ * @list: Linked list to check
+ * Return: 0 if there is no cycle, 1 if there is
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *deffi = list;
+	listint_t *effi = list;
+
+	if (!list)
+		return (0);
+
+	while (deffi && effi && effi->next)
+	{
+		deffi = deffi->next;
+		effi = effi->next->next;
+		if (deffi == effi)
+			return (1);
+	}
+	return (0);
+}
+
