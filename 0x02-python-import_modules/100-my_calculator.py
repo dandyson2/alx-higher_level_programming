97b1b3c08428759a0cdb695f64b2e03624f5d545
dandyson2 <dandysonvictor@yahoo.com>
2023-06-08 11:37:44 -0700

    Build my own calculator, Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
+"""Handle basic arithmetic operations."""
 from calculator_1 import add, sub, mul, div
 import sys
 
-def calculate_result(operator, a, b):
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if operator not in ops:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    return ops[operator](a, b)
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+ops = {"+": add, "-": sub, "*": mul, "/": div}
+operator = sys.argv[2]
+if operator not in ops:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    result = calculate_result(operator, a, b)
-    print("{} {} {} = {}".format(a, operator, b, result))
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+print("{} {} {} = {}".format(a, operator, b, ops[operator](a, b)))
