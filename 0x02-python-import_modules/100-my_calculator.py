74627b911e4c0effdffc8d2a4b397a3a9c948cdb
dandyson2 <dandysonvictor@yahoo.com>
2023-06-08 11:57:35 -0700

    Build my own calculator, Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,17 +2,32 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
 
-a, operator, b = map(int, sys.argv[1:4])
+def validate_arguments():
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-ops = {"+": add, "-": sub, "*": mul, "/": div}
-if operator not in ops:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    operator = sys.argv[2]
+    if operator not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-result = ops[operator](a, b)
-print(f"{a} {operator} {b} = {result}")
+    return int(sys.argv[1]), operator, int(sys.argv[3])
 
+
+def perform_operation(a, operator, b):
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    result = ops[operator](a, b)
+    return result
+
+
+def print_result(a, operator, b, result):
+    print("{} {} {} = {}".format(a, operator, b, result))
+
+
+if __name__ == "__main__":
+    a, operator, b = validate_arguments()
+    result = perform_operation(a, operator, b)
+    print_result(a, operator, b, result)
