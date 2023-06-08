e1395eee9824437ef3d76bb95aaa83132da1cb75
dandyson2 <dandysonvictor@yahoo.com>
2023-06-08 11:52:27 -0700

    Build my own calculator, Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""Handle basic arithmetic operations."""
 from calculator_1 import add, sub, mul, div
 import sys
 
@@ -7,12 +6,13 @@ if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     sys.exit(1)
 
+a, operator, b = map(int, sys.argv[1:4])
+
 ops = {"+": add, "-": sub, "*": mul, "/": div}
-operator = sys.argv[2]
 if operator not in ops:
     print("Unknown operator. Available operators: +, -, * and /")
     sys.exit(1)
 
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-print("{} {} {} = {}".format(a, operator, b, ops[operator](a, b)))
+result = ops[operator](a, b)
+print(f"{a} {operator} {b} = {result}")
+
