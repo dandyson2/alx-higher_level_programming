694b9da4f3c1f8c20cd2e062bfe5c8b6f36ca245
dandyson2 <dandysonvictor@yahoo.com>
2023-06-16 15:35:52 -0700

    CPython, PyBytesObject, Task 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -32,11 +32,9 @@ void print_python_bytes(PyObject *p)
 
 	printf("  first %ld bytes:", limit);
 
-	i = 0;
-	while i < limit;
-	byte = string[i] if string[i] >= 0 else 256 + string[i]
+	for (i = 0; i < limit; i++)
+		if (string[i] >= 0)
 			printf(" %02x", string[i]);
-	i += 1;
 		else
 			printf(" %02x", 256 + string[i]);
 
@@ -48,3 +46,25 @@ void print_python_bytes(PyObject *p)
  * @p: Object of python
  * Return: No return
  */
+
+void print_python_list(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;
+	PyObject *obj;
+
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
+	}
+}
