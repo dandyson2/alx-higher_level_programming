9905e10c8eb7ae5b306b095bc4385ff4804c5532
dandyson2 <dandysonvictor@yahoo.com>
2023-06-27 16:32:43 -0700

    Access and update private attribute, Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,11 +18,8 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = 0
+        self.size = size
 
     @property
     def size(self):
