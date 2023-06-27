7473fff9343f97694c51644941bf52ff08e43869
dandyson2 <dandysonvictor@yahoo.com>
2023-06-26 17:55:25 -0700

    CPython, PyFloatObject, Task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,6 +1,10 @@
 #include <Python.h>
 #include <stdio.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
 * print_python_list - Function that prints some
 * basic info about python list
