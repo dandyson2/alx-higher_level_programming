522dce327d810d59279df503464d11d526e9b39a
dandyson2 <dandysonvictor@yahoo.com>
2023-06-27 05:55:55 -0700

    Size validation, Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
-def __init__(self, size=0):
-    for val in (size,):
-        if not isinstance(val, int):
-            raise TypeError("size must be an integer")
-        if val < 0:
-            raise ValueError("size must be >= 0")
+"""
+A module that defines a square.
+"""
 
-    self.__size = size
+
+class Square:
+    """
+    A class that represents a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes an instance of the Square class.
+
+        Args:
+            size (int): Represents the size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than zero.
+        """
+        for value, message in [(size, 'size must be an integer'),
+                               (size < 0, 'size must be >= 0')]:
+            if not isinstance(value, int):
+                raise TypeError(message)
+            if value:
+                raise ValueError(message)
+
+        self.__size = size
