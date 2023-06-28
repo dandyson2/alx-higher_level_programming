3533b8a49af8f00c279d7093b5286952a598d3fd
dandyson2 <dandysonvictor@yahoo.com>
2023-06-28 00:04:20 -0700

    Access and update private attribute, Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-class Square:
-    """
-    A class that represents a square.
+"""A module that defines a square based on the previous """
 
-    Attributes:
-        __size (int): The size of the square.
-    """
+class Square:
+    """A class that represents a square"""
 
     def __init__(self, size=0):
         """
-        Initializes a Square instance.
+        Initializes an instance of the Square class.
 
         Args:
-            size (int): The size of the square. Default is 0.
+            size (int): Represents the size of the square. Default is 0.
 
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        self.__size = 0
-        self.size = size
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
 
     @property
     def size(self):
@@ -29,6 +30,7 @@ class Square:
         Returns:
             int: The size of the square.
         """
+
         return self.__size
 
     @size.setter
@@ -37,16 +39,17 @@ class Square:
         Sets the size of the square.
 
         Args:
-            value (int): The size value to be set.
+            value (int): The new size for the square.
 
         Raises:
             TypeError: If value is not an integer.
             ValueError: If value is less than 0.
         """
+
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     def area(self):
@@ -54,6 +57,7 @@ class Square:
         Calculates the area of the square.
 
         Returns:
-            int: The square of the size, representing the area.
+            int: The square of the size.
         """
+
         return self.__size ** 2
