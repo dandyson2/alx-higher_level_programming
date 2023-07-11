88c8b33faee20a0e44a7f49de5e49c782298bf0f
dandyson2 <dandysonvictor@yahoo.com>
2023-07-11 02:06:52 -0700

    Rectangle, Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
 """Inherit from BaseGeometry"""
-from typing import Union
 
 
-class BaseGeometry:
-    """Base class for geometry"""
-
-    def area(self) -> float:
-        """Calculates the area"""
-        raise NotImplementedError("area() is not implemented")
-
-    def integer_validator(self, name: str, value: Union[int, float]) -> None:
-        """Validates if value is a positive integer"""
-        if not isinstance(value, (int, float)):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class to define a rectangle using BaseGeometry"""
+    """Module class that defines rectangle using BaseGeometry"""
 
-    def __init__(self, width: int, height: int) -> None:
-        """Initialize a new Rectangle"""
-        super().__init__()
+    def __init__(self, width, height):
+        """Intialize a new Rectangle
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
