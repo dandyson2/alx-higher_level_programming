30d47e0ef618719770a9a2f48dba1bb6a6874700
dandyson2 <dandysonvictor@yahoo.com>
2023-07-11 01:48:50 -0700

    Square number two, Task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,31 +2,17 @@
 """This module defines a Rectangle"""
 
 
-class Rectangle:
-    """Represent a rectangle"""
-
-    def __init__(self, width, height):
-        """Initialize a new rectangle"""
-        self._width = width
-        self._height = height
-
-    def area(self):
-        """Calculate the area of the rectangle"""
-        return self._width * self._height
-
-    def __str__(self):
-        """Return a string representation of the rectangle"""
-        return "Rectangle(width={}, height={})".format(self._width, self._height)
+def __init__(self, width, height):
+    """Initialize a new rectangle"""
+    self.width = width
+    self.height = height
 
 
-class Square(Rectangle):
-    """Represent a square"""
+def area(self):
+    """Calculate the area of the rectangle"""
+    return self.width * self.height
 
-    def __init__(self, size):
-        """Initialize a new square"""
-        super().__init__(size, size)
-        self._size = size
 
-    def __str__(self):
-        """Return a string representation of the square"""
-        return "Square(side={})".format(self._size)
+def perimeter(self):
+    """Calculate the perimeter of the rectangle"""
+    return 2 * (self.width + self.height)
