1e023645dedd9716535048c4dfc510370b3c3c26
dandyson2 <dandysonvictor@yahoo.com>
2023-07-12 07:33:00 -0700

    Write to a file, Task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module a function that writes a string to a text file
+
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF-8 text file"""
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
