3ac2cf163e0d5239b077dd4bf963efde98f7c1fe
dandyson2 <dandysonvictor@yahoo.com>
2023-07-12 10:22:27 -0700

    Student to JSON with filter, Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,11 +7,11 @@
 def append_after(filename="", search_string="", new_string=""):
     """Inserts text after each line containing a given string in a file
     """
-    page = ""
+    text = ""
     with open(filename) as r:
         for line in r:
-            page += line
+            text += line
             if search_string in line:
-                page += new_string
+                text += new_string
     with open(filename, "w") as w:
-        w.write(page)
+        w.write(text)
