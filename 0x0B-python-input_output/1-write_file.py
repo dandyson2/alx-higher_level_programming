e6a538605172de24d38c7356d98847f8fa593405
dandyson2 <dandysonvictor@yahoo.com>
2023-07-12 07:41:02 -0700

    Write to a file, Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,10 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a UTF-8 text file"""
-    with open(filename, "w", encoding="utf-8") as file:
-        file.write(text)
-        return f.write(text)
+    """Writes a string to a UTF-8 text file and returns the number of chars"""
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            num_chars_written = file.write(text)
+            return num_chars_written
+    except IOError:
+        return 0
