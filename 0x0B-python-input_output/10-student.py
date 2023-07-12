2f761692cf21eb86cfa3f83d6c5eac25e6e294c0
dandyson2 <dandysonvictor@yahoo.com>
2023-07-12 10:37:55 -0700

    Student to JSON with filter, Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,14 +4,27 @@
 """
 
 
-def append_after(filename="", search_string="", new_string=""):
-    """Inserts text after each line containing a given string in a file
-    """
-    text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
-            if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the Student.
+
+        If attrs is a list of strings, only the attributes in the list are rep
+        """
+        if (
+            type(attrs) == list
+            and all(type(ele) == str for ele in attrs)
+        ):
+            return {
+                k: getattr(self, k)
+                for k in attrs
+                if hasattr(self, k)
+            }
+        return self.__dict__
