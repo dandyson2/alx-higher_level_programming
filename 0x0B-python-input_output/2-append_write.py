07cc9c568300b54de76c727768ea74ba1e342552
dandyson2 <dandysonvictor@yahoo.com>
2023-07-12 08:05:56 -0700

    Append to a file, Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""This module a function that appends a string at the end of a taxt file.
+"""This module a function that appends a string at the end of a text file
 
 """
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF-8 text file"""
-    with open(filename, "a", encoding="utf-8") as file:
-        file.write(text)
+    """Function that appends a string to the end of UTF8 text file
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
