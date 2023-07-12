f14c73bd5c56fbb30c7462eba22e8e7753ea31b0
dandyson2 <dandysonvictor@yahoo.com>
2023-07-12 11:55:23 -0700

    Search and update, Task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines a text file insertion function
+
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts text after each line containing a given string in a file"""
+    lines = []
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    with open(filename, "w") as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
