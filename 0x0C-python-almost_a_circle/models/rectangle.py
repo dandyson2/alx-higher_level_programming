5e3f87fe72d8795aae47e2749c2b9d399e9d4783
dandyson2 <dandysonvictor@yahoo.com>
2023-07-14 02:37:44 -0700

    First Rectangle, Task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+"""
+    This module defines a class rectangle that inherits from Base
+
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Represents a rectangle object that inherits from Base.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle's position.
+            y (int, optional): The y-coordinate of the rectangle's position.
+            id (int, optional): The ID of the rectangle. Defaults to None.
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """
+        Get the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            value (int): The new width of the rectangle.
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Get the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set the height of the rectangle.
+
+        Args:
+            value (int): The new height of the rectangle.
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Get the x-coordinate of the rectangle's position.
+
+        Returns:
+            int: The x-coordinate of the rectangle's position.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Set the x-coordinate of the rectangle's position.
+
+        Args:
+            value (int): The new x-coordinate of the rectangle's position.
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Get the y-coordinate of the rectangle's position.
+
+        Returns:
+            int: The y-coordinate of the rectangle's position.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Set the y-coordinate of the rectangle's position.
+
+        Args:
+            value (int): The new y-coordinate of the rectangle's position.
+        """
+        self.__y = value
