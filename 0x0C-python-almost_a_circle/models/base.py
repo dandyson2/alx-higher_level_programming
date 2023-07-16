35856e88a627366a587dd46cfe480f8f3a29355b
dandyson2 <dandysonvictor@yahoo.com>
2023-07-16 16:59:07 -0700

    Dictionary to JSON string, Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,14 @@ class Base:
             # Increment __nb_objects and assign the new value to id
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the string "[]" if list_dic is None or empty, otherwise JSON
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
