bb30c8c78815653ce07f81900b28e6fa65b277b8
dandyson2 <dandysonvictor@yahoo.com>
2023-07-17 00:30:13 -0700

    Let's draw it, Task 21

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -142,3 +142,50 @@ class Base:
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
             return []
+
+    @staticmethod
+def draw(list_rectangles, list_squares):
+    """Module that opens a window and draws all the Rectangles and Squares
+
+    Args:
+        list_rectangles (list): A list of Rectangle objects to draw.
+        list_squares (list): A list of Square objects to draw.
+    """
+    turt = turtle.Turtle()
+    turt.screen.bgcolor("#b7312c")
+    turt.pensize(3)
+    turt.shape("turtle")
+
+    turt.color("#ffffff")
+    index = 0
+    while index < len(list_rectangles):
+        rect = list_rectangles[index]
+        turt.showturtle()
+        turt.up()
+        turt.goto(rect.x, rect.y)
+        turt.down()
+        for i in range(2):
+            turt.forward(rect.width)
+            turt.left(90)
+            turt.forward(rect.height)
+            turt.left(90)
+        turt.hideturtle()
+        index += 1
+
+    turt.color("#b5e3d8")
+    index = 0
+    while index < len(list_squares):
+        sq = list_squares[index]
+        turt.showturtle()
+        turt.up()
+        turt.goto(sq.x, sq.y)
+        turt.down()
+        for i in range(2):
+            turt.forward(sq.width)
+            turt.left(90)
+            turt.forward(sq.height)
+            turt.left(90)
+        turt.hideturtle()
+        index += 1
+
+    turtle.exitonclick()
