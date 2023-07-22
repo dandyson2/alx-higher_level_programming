90781e4e35d1bf6d490e962b0808bcaad1bdac89
dandyson2 <dandysonvictor@yahoo.com>
2023-07-22 01:22:10 -0700

    And now, the Square, Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Module that contains class Square, implements class Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+        Square implements rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+            initialises Square (overrides Rectangle init)
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """
+            Returns size of square
+        """
+        return self.width
