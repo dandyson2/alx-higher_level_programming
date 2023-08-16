cf61305060da6a4cabcf6b0268be5e05df62752c
dandyson2 <dandysonvictor@yahoo.com>
2023-08-16 15:53:22 -0700

    My privileges, Task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 --  a script that lists all priviledges of the MYSQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT * FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2');
+
