00935644cdc4753de372b8003deb2ba99f6a39c0
dandyson2 <dandysonvictor@yahoo.com>
2023-08-16 23:35:00 -0700

    My privileges, Task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,14 +1,3 @@
 --  a script that lists all priviledges of the MYSQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
-REVOKE ALL ON *.* FROM 'user_0d_1'@'localhost';
-
--- Grant missing privileges (replace 'privilege_name' with the actual privilege to grant)
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
-
--- Apply the changes
-FLUSH PRIVILEGES;
-
--- Exit MySQL
-EXIT;
