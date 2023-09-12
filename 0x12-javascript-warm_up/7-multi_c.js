bfe8c6c967eafe7a59434c4687bdf262278e266c
dandyson2 <dandysonvictor@yahoo.com>
2023-09-12 03:17:58 -0700

    I love C, Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
 
-if (args.length === 0) {
-  console.log("Missing number of occurrences");
+const numberOfOccurrences = Math.floor(Number(process.argv[2]));
+
+if (isNaN(numberOfOccurrences)) {
+  console.log('Missing number of occurrences');
 } else {
-  const x = parseInt(args[0]);
-
-  if (isNaN(x)) {
-    console.log("Missing number of occurrences");
-  } else {
-    for (let i = 0; i < x; i++) {
-      console.log("C is fun");
-    }
+  for (let i = 0; i < numberOfOccurrences; i++) {
+    console.log('C is fun');
   }
 }
