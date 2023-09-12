022b40669740543135ba6a37570dd24b8509f7ea
dandyson2 <dandysonvictor@yahoo.com>
2023-09-12 04:20:47 -0700

    Factorial, Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
-function factorial(n) {
-  if (n === 0 || isNaN(n)) {
-    return 1;
-  }
-  return n * factorial(n - 1);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-const inputNumber = Number(process.argv[2]);
-
-if (!isNaN(inputNumber)) {
-  console.log(factorial(inputNumber));
-} else {
-  console.error("Invalid input. Please provide a valid number.");
-}
+console.log(factorial(Number(process.argv[2])));
