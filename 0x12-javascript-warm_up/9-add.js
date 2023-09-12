50373a6966ba1d2958a9dabb3e38414d2363b07b
dandyson2 <dandysonvictor@yahoo.com>
2023-09-12 03:51:45 -0700

    Add, Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
-const arg1 = Number(process.argv[2]);
-const arg2 = Number(process.argv[3]);
-
-console.log(add(arg1, arg2));
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
