ff211f98774cbdc9781884f1ed8634b76408313d
dandyson2 <dandysonvictor@yahoo.com>
2023-09-12 03:43:40 -0700

    Square, Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
-
 const size = Math.floor(Number(process.argv[2]));
 
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let y = 0; y < size; y++) {
-    let row = 'X'.repeat(size);
+  let y = 0;
+  while (y < size) {
+    let row = '';
+    let z = 0;
+    while (z < size) {
+      row += 'X';
+      z++;
+    }
     console.log(row);
+    y++;
   }
 }
