940c923aa67ccaab3593c9efba529159d4dca4c3
dandyson2 <dandysonvictor@yahoo.com>
2023-09-11 18:11:38 -0700

    3 languages, Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Define a variable with a multiline string.
+const myVar = `
+C is fun,
+Python is cool,
+JavaScript is amazing
+`;
+
+// Print the value of the variable to the console.
+console.log(myVar);
