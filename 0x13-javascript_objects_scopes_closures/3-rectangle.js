d6cba78211bda8067f12c27886cfd0ee377be2d4
dandyson2 <dandysonvictor@yahoo.com>
2023-09-12 21:06:26 -0700

    Rectangle #3, Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
+#!/usr/bin/node
 module.exports = class Rectangle {
   constructor(w, h) {
     if (w > 0 && h > 0) {
-      [this.width, this.height] = [w, h];
+      this.width = w;
+      this.height = h;
     }
   }
 
   print() {
-    let i = 0;
-    while (i < this.height) {
-      console.log('X'.repeat(this.width));
-      i++;
+    if (this.width && this.height) {
+      console.log('X'.repeat(this.width).repeat(this.height));
     }
   }
 };
