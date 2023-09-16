b37d0e02fb133fb25107ba6eab274364f7e42f42
dandyson2 <dandysonvictor@yahoo.com>
2023-09-16 02:57:41 -0700

    Filter states, Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,21 +6,25 @@ from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
 
-def list_states_with_letter_n(username, password, database_name):
-    """
-    Access the database and retrieve the states
-    with names starting with 'N'.
-    """
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        return
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
     try:
-        db = MySQLdb.connect(host="localhost", user=username, port=3306,
-                             passwd=password, db=database_name)
+        db = MySQLdb.connect(
+            host="localhost", user=username, port=3306,
+            passwd=password, db=database
+        )
 
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name .. 'N%' ORDER BY id ASC")
-        rows = cur.fetchall()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE name 'N%' ORDER BY id ASC")
+        rows = cursor.fetchall()
 
         for row in rows:
             print(row)
@@ -33,8 +37,4 @@ def list_states_with_letter_n(username, password, database_name):
 
 
 if __name__ == '__main__':
-    if len(argv) != 4:
-        print("Usage: ./script.py <username> <password> <database_name>")
-    else:
-        username, password, database_name = argv[1], argv[2], argv[3]
-        list_states_with_letter_n(username, password, database_name)
+    main()
