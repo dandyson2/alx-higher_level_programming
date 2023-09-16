8b08eee6d79d40f279685b0b12347d1a63b55d72
dandyson2 <dandysonvictor@yahoo.com>
2023-09-16 02:15:05 -0700

    Get all states, Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """
 This script lists all states from the database `hbtn_0e_0_usa`.
+
 """
 
-import sys
+
 import MySQLdb
+from sys import argv
 
 def main():
     """
     Access the database and retrieve the states.
+
     """
+
+
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
