9122500c73891f51aa0e853d06d189485cc2f1f9
dandyson2 <dandysonvictor@yahoo.com>
2023-09-17 16:25:11 -0700

    From city, Task 17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Lists all City objects from the database hbtn_0e_101_usa
+This script lists all City objects from the database hbtn_0e_101_usa
 """
+
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -9,27 +10,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-def main():
-    # Create a database connection
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
-    )
+if __name__ == '__main__':
+    # Create a connection to the MySQL database using command-line arguments
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    # Create a session
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and retrieve states with associated cities
+    # Query the database to retrieve all State objects and their asso City obj
     states_with_cities = session.query(State).join(City).order_by(City.id)
     .all()
 
-    # Print the cities and their associated states
+    # Iterate through the results and print City information along with states
     for state in states_with_cities:
         for city in state.cities:
             print("{}: {} -> {}".format(city.id, city.name, state.name))
-
-
-if __name__ == '__main__':
-    main()
