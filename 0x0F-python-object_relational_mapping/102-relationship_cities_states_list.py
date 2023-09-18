45242cabf93d5cc6401d228e8bea8a773d8cf75b
dandyson2 <dandysonvictor@yahoo.com>
2023-09-17 17:15:22 -0700

    From city, Task 17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-This script lists all City objects from the database hbtn_0e_101_usa
+Lists all City objects from the database hbtn_0e_101_usa
 """
-
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -10,20 +9,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == '__main__':
-    # Create a connection to the MySQL database using command-line arguments
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    # Create a session to interact with the database
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Create the database engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, database),
+                           pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the database to retrieve all State objects and their asso City obj
-    states_with_cities = session.query(State).join(City).order_by(City.id)
-    .all()
+    # Query and print City objects
+    cities_and_states = session.query(City, State).join(State)
+    .order_by(City.id).all()
 
-    # Iterate through the results and print City information along with states
-    for state in states_with_cities:
-        for city in state.cities:
-            print("{}: {} -> {}".format(city.id, city.name, state.name))
+    for city, state in cities_and_states:
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
+
+
+if __name__ == '__main__':
+    main()
