b5bd37a51f55d2a2408523cb995ce68bfd293ce5
dandyson2 <dandysonvictor@yahoo.com>
2023-09-28 19:48:55 -0700

    Find a peak, Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(input_list):
+    """
+    Args:
+        input_list(list): list of integers to find peak of
+    Returns: peak of input_list or None
+    """
+    size = len(input_list)
+    
+    if size == 0:
+        return None
+
+    mid = size // 2
+
+    for _ in range(size):
+        mid_e = mid // 2
+        
+        if (mid < size - 1 and input_list[mid] < input_list[mid + 1]):
+            mid += mid_e if mid_e != 0 else 2
+        elif mid_e > 0 and input_list[mid] < input_list[mid - 1]:
+            mid -= mid_e if mid_e != 0 else 2
+        else:
+            return input_list[mid]
