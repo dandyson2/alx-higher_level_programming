5b4bb7688334e61282a4e3b8592b1883950bebd7
dandyson2 <dandysonvictor@yahoo.com>
2023-09-29 17:14:44 -0700

    Response header value, Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the value
+"""Python script that takes in a URL, and send a request to URL
 """
 import sys
 import urllib.request
