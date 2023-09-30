57fa3b738bfe41d60e96d944e3e462482def2dc7
dandyson2 <dandysonvictor@yahoo.com>
2023-09-29 18:41:10 -0700

    Time for an interview, Task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
+"""Python script that takes 2 arguments: repo name and owner name in other ...
 """
-Python script that takes 2 arguments: repo name and owner name to solve ...
-"""
-
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    # Construct the GitHub API URL with repository owner and name
-    repository_owner = sys.argv[2]
-    repository_name = sys.argv[1]
-    url = f"https://api.github.com/repos/{repository_owner}/\
-    {repository_name}/commits"
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    # Send a GET request to the GitHub API
-    response = requests.get(url)
-    commits = response.json()
-
+    r = requests.get(url)
+    commits = r.json()
     try:
-        # Print the 10 most recent commits and their authors
         for i in range(10):
-            sha = commits[i].get("sha")
-            author_name = commits[i].get("commit").get("author").get("name")
-            print(f"{sha}: {author_name}")
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
-        # Handle the case where there are fewer than 10 commits
         pass
