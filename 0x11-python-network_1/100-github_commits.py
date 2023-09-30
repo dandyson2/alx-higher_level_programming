bf09d3c635d1334b6d4d83fc3e1e6adb2efc2ee1
dandyson2 <dandysonvictor@yahoo.com>
2023-09-29 18:34:47 -0700

    Time for an interview, Task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""Python script that takes 2 arguments, repo name and owner name for the ...
+"""
+Python script that takes 2 arguments: repo name and owner name to solve ...
 """
 
 import sys
 import requests
 
-
-def get_recent_commits(repository_name, repository_owner):
+if __name__ == "__main__":
+    # Construct the GitHub API URL with repository owner and name
+    repository_owner = sys.argv[2]
+    repository_name = sys.argv[1]
     url = f"https://api.github.com/repos/{repository_owner}/\
     {repository_name}/commits"
+
+    # Send a GET request to the GitHub API
     response = requests.get(url)
     commits = response.json()
+
     try:
+        # Print the 10 most recent commits and their authors
         for i in range(10):
             sha = commits[i].get("sha")
             author_name = commits[i].get("commit").get("author").get("name")
             print(f"{sha}: {author_name}")
     except IndexError:
+        # Handle the case where there are fewer than 10 commits
         pass
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./script.py <repository name> <repository owner>")
-    else:
-        repository_name = sys.argv[1]
-        repository_owner = sys.argv[2]
-        get_recent_commits(repository_name, repository_owner)
