a4a0d510a383a1a0dfc4a6b9f693b3081d2497ff
dandyson2 <dandysonvictor@yahoo.com>
2023-09-29 18:02:04 -0700

    Search API, Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request ...
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    payload = {"q": letter}
+
+    response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    try:
+        data = response.json()
+        if not data:
+            print("No result")
+        else:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+    except ValueError:
+        print("Invalid JSON format in response")
