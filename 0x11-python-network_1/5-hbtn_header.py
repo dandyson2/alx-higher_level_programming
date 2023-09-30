8337de9e16f348e96e1fe2dbf039c099a694ca8b
dandyson2 <dandysonvictor@yahoo.com>
2023-09-29 17:43:53 -0700

    Response header value, Task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,9 +6,11 @@ Python script that takes in a URL, sends a request to the URL and displays ...
 import sys
 import requests
 
+
 def get_x_request_id(url):
     response = requests.get(url)
     return response.headers.get("X-Request-Id")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
