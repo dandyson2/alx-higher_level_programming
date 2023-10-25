7a22ff2cd8357d1f5cb960c2a5a7d2ae428e6823
dandyson2 <dandysonvictor@yahoo.com>
2023-10-25 06:14:41 -0700

    Star wars movie title, Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,26 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+
+const filmId = process.argv[2];
+const url = `http://swapi.co/api/films/${filmId}`;
+
+const fetchData = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
+
+fetchData(url)
+  .then((body) => {
+    const filmData = JSON.parse(body);
+    console.log(filmData.title);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
