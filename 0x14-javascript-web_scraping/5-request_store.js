4be6c01164aa468cbe8b7cae78960c3e81090e2e
dandyson2 <dandysonvictor@yahoo.com>
2023-10-25 19:30:02 -0700

    Loripsum, Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,27 +2,4 @@
 
 const fs = require('fs');
 const request = require('request');
-
-const fetchAndSaveWebpage = (url, filePath) => {
-    request(url, (error, response, body) => {
-        if (error) {
-            console.error(`Error: ${error}`);
-        } else {
-            fs.writeFile(filePath, body, 'utf-8', (err) => {
-                if (err) {
-                    console.error(`Error writing to file: ${err}`);
-                } else {
-                    console.log(`Webpage content successfully saved to: ${filePath}`);
-                }
-            });
-        }
-    });
-};
-
-const [, , url, filePath] = process.argv;
-
-if (url && filePath) {
-    fetchAndSaveWebpage(url, filePath);
-} else {
-    console.error('Usage: node script.js <URL> <FilePath>');
-}
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
