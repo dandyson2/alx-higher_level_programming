0b4d7c6fb7ed2c7928f756d0f6c73e62bbfeef19
dandyson2 <dandysonvictor@yahoo.com>
2023-10-25 19:52:15 -0700

    Star wars movie title, Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,15 @@
 #!/usr/bin/node
 
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+
+const movieID = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request(apiUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        const movieData = JSON.parse(body);
+        console.log(`Title of Star Wars Episode ${movieID}: ${movieData.title}`);
+    } else {
+        console.log('Error fetching data from the API.');
+    }
 });
