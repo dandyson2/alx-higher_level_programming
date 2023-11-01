2b2907919f777ab708f6b1d587ed5b6b7f24a7c2
dandyson2 <dandysonvictor@yahoo.com>
2023-11-01 14:03:00 -0700

    No jQuery - document loaded, Task 10

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// Wait for the DOM content to be fully loaded and ready
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the first <header> element and change its text color to red (#FF0000)
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
