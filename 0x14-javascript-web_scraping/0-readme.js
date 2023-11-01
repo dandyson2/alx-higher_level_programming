f4953ac7396a51390f069f336824b425e8983730
dandyson2 <dandysonvictor@yahoo.com>
2023-11-01 04:05:52 -0700

    ok

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
+def read_and_print_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            print(content)
+    except Exception as e:
+        print(f"Error occurred: {e}")
 
-const fs = require('fs');
-
-function readFileContent(filePath) {
-  fs.readFile(filePath, 'utf8', (error, content) => {
-    console.log(error || content);
-  });
-}
-
-const filePath = process.argv[2];
-readFileContent(filePath);
+# Check if the correct number of arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: python script_name.py <file_path>")
+else:
+    file_path = sys.argv[1]
+    read_and_print_file(file_path)
